87f522e509f0ce3a515cd057a4cce00d4cbd2478
Alfred024 <21030761@itcelaya.edu.mx>
2022-06-12 18:59:30 -0500

    Primer método

---
+++ forEach.js
@@ -0,0 +1,7 @@
+const arrayChars = [`a`,`b`,`c`];
+arrayChars.forEach(elemento => console.log(elemento));
+
+const arrayNumbers = [20,19,15];
+arrayNumbers.forEach(
+    elemento => console.log(elemento)
+);
