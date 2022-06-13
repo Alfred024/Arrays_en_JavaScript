9a0bf677305b8a4e24e5926a1d6ae4840e3319b0
Alfred024 <21030761@itcelaya.edu.mx>
2022-06-12 20:03:30 -0500

    Clase forEach

--- forEach.js
+++ forEach.js
@@ -1,7 +1,26 @@
-const arrayChars = [`a`,`b`,`c`];
-arrayChars.forEach(elemento => console.log(elemento));
 
+//-----------CLASE-----------//
 const arrayNumbers = [20,19,15];
 arrayNumbers.forEach(
     elemento => console.log(elemento)
+);
+
+const listaProductos = document.getElementById("listaProductos");
+
+arrayNumbers.forEach(producto => 
+    {listaProductos.innerHTML += `<li>${"Edad: "+producto}</li>`;}
+);
+
+
+//----------RETO----------//
+const arrayMaterias = [
+    {name: "Cálculo", state: true},
+    {name: "Física", state: true},
+    {name: "Alemán 3", state: false},
+]
+
+const listaMaterias = document.getElementById("listaMaterias");
+
+arrayMaterias.forEach(materia => 
+    {listaMaterias.innerHTML += `${"Materia: "+materia.name}<br>${"Estado: "+materia.state}<br><br>`;}
 );
